b165e3b50fc836cac195bfed3e99bc245ed80f2d
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-11-07 23:01:47 +0500

    cards components added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../App.css";
-// import Cards from "../Cards";
+import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 // import Footer from "../Footer";
 
@@ -8,6 +8,8 @@ function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
+      {/* <Footer /> */}
     </>
   );
 }
