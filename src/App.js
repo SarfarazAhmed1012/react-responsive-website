63b1d3dd76ca948d6bf2e7227fe472d97b6bb153
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-11-07 20:54:06 +0500

    Hero Component added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {} from "react-dom";
 import "./App.css";
 import Navbar from "./components/NavBar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Home />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Routes>
       </Router>
     </>
