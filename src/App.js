c4bb78569c63620b06f253774d1bfb405cc9aa8a
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-11-08 00:18:17 +0500

    Footer Component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {} from "react-dom";
-import "./App.css";
 import Navbar from "./components/NavBar";
+import "./App.css";
 import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Routes } from "react-router";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
         <Navbar />
         <Home />
         <Routes>
-          <Route path="/" exact component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route path="services" component={Services} />
+          <Route path="products" component={Products} />
+          <Route path="sign-up" component={SignUp} />
         </Routes>
       </Router>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
+import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 // import Footer from "../Footer";
 
@@ -9,6 +10,7 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
+      <Footer />
       {/* <Footer /> */}
     </>
   );
