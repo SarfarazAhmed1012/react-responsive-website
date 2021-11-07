aba65adb5a43ebfa7f4e06966f6268de43e18ccc
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-11-07 19:36:02 +0500

    navbar and button component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {} from "react-dom";
 import "./App.css";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <hi>YO</hi>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
